208b154562d7090120187cf9498a90a276de4fd5
ABolikov <Brolond@mail.ru>
2020-12-16 02:03:10 +0600

    Написание UI теста в администри. Проверка меню и активации пунктов меню при выборе. Начал написание теста по добавлению Категории товара.

--- shop-admin-ui-test/src/test/java/org/abolikov/shop/steps/LoginSteps.java
+++ shop-admin-ui-test/src/test/java/org/abolikov/shop/steps/LoginSteps.java
@@ -1,14 +1,20 @@
 package org.abolikov.shop.steps;
 
+import com.google.common.collect.ArrayListMultimap;
 import cucumber.api.java.After;
 import cucumber.api.java.en.And;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.abolikov.shop.DriverInitializer;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -74,7 +80,7 @@ public class LoginSteps {
         webElement.click();
     }
 
-    @Then("Check logout")
+    @Then("^Check logout$")
     public void checkLogout() {
         WebElement webElement = webDriver.findElement(By.id("dd_user"));
         assertThat(webElement.getText()).isEqualTo("");
@@ -83,6 +89,51 @@ public class LoginSteps {
     @After
     public void quitBrowser() {
         webDriver.quit();
+    }
+
+
+    @Then("^Check menu admin$")
+    public void checkMenuAdmin() {
+        ArrayList<String> check = new ArrayList<>();
+        check.add("Пользователи");
+        check.add("Роли пользователей");
+        check.add("Продукты");
+        check.add("Категории продуктов");
+        check.add("Бренды");
+        List<WebElement> webElements = webDriver.findElement(By.className("list-unstyled")).findElements(By.tagName("li"));
+        webElements.forEach(webElement -> Assert.assertTrue("Не найден элемент " + webElement.getText(),
+                check.contains(webElement.getText())));
+    }
+
+    @And("^check active menu$")
+    public void checkActiveMenu() {
+        List<WebElement> webElements = webDriver.findElement(By.className("list-unstyled")).findElements(By.tagName("li"));
+        int size = webElements.size();
+        for (int i = 0; i <= size - 1; i++) {
+            if (i > 0)
+                webElements = webDriver.findElement(By.className("list-unstyled")).findElements(By.tagName("li"));
+            String name = webElements.get(i).getText();
+            webElements.get(i).click();
+            WebElement element = webDriver.findElement(By.className("list-unstyled")).findElement(By.className("active"));
+            Assert.assertEquals("Пункт меню " + name + " не выделен как активный", name, element.getText());
+        }
+    }
+
+    @Then("^I click menu category$")
+    public void iClickMenuCategory() {
+        List<WebElement> webElements = webDriver.findElement(By.className("list-unstyled")).findElements(By.tagName("li"));
+        webElements.get(3).click();
+        Assert.assertTrue("Не выполнен переход на страницу \"Категории продуктов\"",
+                webDriver.getCurrentUrl().contains("admin/categories"));
+    }
+
+    @And("^I click button add new category$")
+    public void iClickButtonAddNewCategory() throws InterruptedException {
+        Thread.sleep(2000);
+        WebElement element = webDriver.findElement(By.linkText("/admin/categories/add"));
+        element.click();
+        Assert.assertTrue("Не выполнен переход на страницу добавления категории \"Категории продуктов\"",
+                webDriver.getCurrentUrl().contains("admin/categories/add"));
     }
 
 
